14478307c18adba81417215cd4f5d71857c78499
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-05-28 20:30:52 -0300

    icons showing ok

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,6 +1,7 @@
 import {BsHouseFill, BsBellFill} from "react-icons/bs"
 import { FaUser } from "react-icons/fa";
 import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
     const items = [
@@ -27,7 +28,18 @@ const Sidebar = () => {
     <div className="col-span-1 pr-4 md:pr-6">
         <div className="flex flex-col items-end ">
                 <div className="space-y-2 lg:w-[230px]">
-                        <SidebarLogo/>
+                    <SidebarLogo />
+                   {items.map((item) => {
+    
+    return (
+        <SidebarItem
+            key={item.href}
+            href={item.href}
+            label={item.label}
+            icon={item.icon}
+        />
+    );
+})}
                 </div> 
         </div>
     </div>

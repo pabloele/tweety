b0f0d822cbf1fad003f17250f27574bcd9eb2075
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-05-29 13:02:16 -0300

    button component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import Layout from '@/components/Layout'
+import Modal from '@/components/Modal'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-        <Layout>
-      
-          <Component {...pageProps} />
+    <>
+      <Modal/>
+    <Layout>
+                <Component {...pageProps} />
         </Layout>
 
+    
+    </>
+        
   )
   
   
